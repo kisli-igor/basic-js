c6844e1dfd0e4613766794abf97357760637b277
kisli-igor <kisiigor@gmail.com>
2021-03-27 22:04:29 +0200

    feat: dreamTeamGenerator added

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  if (Array.isArray(team)){
+      let dreamTeamName = [];
+      team.forEach( person => {
+
+        if (typeof(person) === "string"){
+          dreamTeamName.push(person.trim()[0].toUpperCase())
+        }
+      })
+      return dreamTeamName.sort().join("")
+  }
+  return false
 };
