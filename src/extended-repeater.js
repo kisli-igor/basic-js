e35a7ef405e2ee1db35706c171ccc0355a39986f
kisli-igor <kisiigor@gmail.com>
2021-03-29 21:59:53 +0300

    feat: repeater repeated

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  let {repeatTimes = 1,
+       separator = '+',
+       addition ='',
+       additionRepeatTimes = 1,
+       additionSeparator = '|'} = options;
+
+  return (String(str) + (String(addition) + additionSeparator).repeat(additionRepeatTimes).slice(0, -additionSeparator.length) + separator).repeat(repeatTimes).slice(0, -separator.length)
+
+  }
