2914d199da2cae795e817c07a03db913418c36be
kisli-igor <kisiigor@gmail.com>
2021-03-29 22:39:23 +0300

    feat: ciphered

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine){
+    this.machine = machine
   }
-}
+
+  encrypt(str, key) {
+    if(str == undefined || key == undefined){
+      throw new Error;
+    } else {
+      str = str.toUpperCase();
+      key = key.toUpperCase();
+      let encryptedStr = []
+      const alphabet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let count = 0
+      for (let x = 0; x < str.length; x++){
+        if (alphabet.includes(str[x])){
+          encryptedStr.push(alphabet [(alphabet.indexOf(str[x]) + alphabet.indexOf(key[count % key.length])) % alphabet.length])
+          count += 1;
+        } else {
+          encryptedStr.push(str[x])
+        }
+      } return this.machine !== false ? encryptedStr.join("") : encryptedStr.reverse().join("")
+    }
+  }
+  decrypt(str, key) {
+      if(str == undefined || key == undefined){
+        throw new Error;
+      } else {
+        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let decrypted = [];
+        let count = 0;
+        for (let x = 0; x < str.length; x++) {
+          if (alphabet.includes(str[x])) {
+            decrypted.push(alphabet[(26 + alphabet.indexOf(str[x]) - alphabet.indexOf(key[count % key.length]) ) % alphabet.length]);
+            count += 1;
+          } else {
+            decrypted.push(str[x]);
+          }
+        }
+        return this.machine !== false ? decrypted.join('') : decrypted.reverse().join('');
+      }
+    }
+
+    }
 
 module.exports = VigenereCipheringMachine;
