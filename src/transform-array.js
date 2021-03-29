8661bb54c594498f3938d34b5a375c1713801076
kisli-igor <kisiigor@gmail.com>
2021-03-29 16:57:47 +0300

    feat: transformer implemented

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw Error()
+
+  let transformedArr = arr.map( el => el);
+
+  for (let index = 0; index < transformedArr.length; index++){
+    if (transformedArr[index] == '--discard-next'){
+      if (index == transformedArr.length - 1) {
+        transformedArr.splice(index,1)
+        index--;
+      }
+      else if (transformedArr[index+2] == '--discard-prev' || transformedArr[index+2] == '--double-prev'){
+        transformedArr.splice(index, 3)
+        index--;
+      } else {
+        transformedArr.splice(index, 2)
+        index--;
+      }
+    }
+    else if (transformedArr[index] == '--discard-prev'){
+      if (index == 0){
+        transformedArr.splice(index,1)
+        index--;
+      } else {
+        transformedArr.splice(index-1, 2)
+        index--;
+      }
+    }
+    else if (transformedArr[index] == '--double-next'){
+      if (index == transformedArr.length - 1){
+        transformedArr.splice(index,1)
+        index--;
+      } else {
+        transformedArr[index] = transformedArr[index+1];
+      }
+    }
+    else if (transformedArr[index] == '--double-prev'){
+      if (index == 0){
+        transformedArr.splice(index,1)
+        index--;
+      } else {
+        transformedArr[index] = transformedArr[index-1];
+      }
+    }
+    else continue
+  }
+  return transformedArr;
 };
